ea10754ac6f77877c9fe5b2a0045fd0733987555
c14h19no2 <ningkai.wang.1993@gmail.com>
2024-03-21 00:27:22 +0100

    fix eyetracking setting

--- Experiment/main.py
+++ Experiment/main.py
@@ -3,6 +3,11 @@
 """
 Created on Mon Feb 25 14:04:44 2019
 
+example usage:
+conda activate exptools2
+cd .../Experiment
+python './main.py' sub-002 ses-1 run-1 1
+
 @author: marcoaqil
 Adapted by Ralph Wientjens(ralphw713@hotmail.com)
 """
@@ -10,27 +15,37 @@ import sys
 import os
 from session import PRFSession
 from datetime import datetime
-datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 
 def main():
     subject = sys.argv[1]
-    sess =  sys.argv[2]
+    sess = sys.argv[2]
     # We would do 5 runs, which are loaded individually
     run = sys.argv[3]
-    eyetracker_on = bool(sys.argv[4])
-    
-    output_str= subject+'_'+sess+'_'+run
-    
-    output_dir = './logs/'+output_str+'_Logs'
-    
-    if os.path.exists(output_dir):
-        print("Warning: output directory already exists. Renaming to avoid overwriting.")
-        output_dir = output_dir + datetime.now().strftime('%Y%m%d%H%M%S')
-    
-    settings_file='./expsettings_2R.yml'
+    eyetracker_on = bool(int(sys.argv[4]))
 
-    ts = PRFSession(output_str=output_str, output_dir=output_dir, settings_file=settings_file, eyetracker_on=eyetracker_on)
+    output_str = subject + "_" + sess + "_" + run
+
+    output_dir = "./logs/" + output_str + "_Logs"
+
+    if os.path.exists(output_dir):
+        print(
+            "Warning: output directory already exists. Renaming to avoid overwriting."
+        )
+        output_dir = output_dir + datetime.now().strftime("%Y%m%d%H%M%S")
+
+    settings_file = "./expsettings_2R.yml"
+
+    ts = PRFSession(
+        output_str=output_str,
+        output_dir=output_dir,
+        settings_file=settings_file,
+        eyetracker_on=eyetracker_on,
+    )
     ts.run()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
